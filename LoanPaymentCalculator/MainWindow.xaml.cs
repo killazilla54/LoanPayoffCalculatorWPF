cb84c76d72c6aacb508abd14f4aa75f12cdbe033
Kevin <killazilla54@users.noreply.github.com>
2020-10-16 01:08:45 -0400

    Initial Commit

---
+++ LoanPaymentCalculator/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using LoanPaymentCalculator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows;
+
+
+namespace LoanPaymentCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Loan> Loans { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loans = new ObservableCollection<Loan>();
+            //Init 1st row of loan fields
+            this.Loans.Add(new Loan(1));
+            icLoansList.ItemsSource = this.Loans;
+        }
+
+        /*
+         * Adds a new empty row of loan input fields
+         */
+        private void AddRowBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("Add Button Clicked");
+            this.Loans.Add(new Loan(Loans.Count+1));
+        }
+
+        private void CalcBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(" Calc Button Clicked");
+            ResultsText.Content = "";
+            Dictionary<int, double> totalLoans = new Dictionary<int, double>();
+
+            List<Loan> payoffLoans = new List<Loan>();
+            foreach(Loan l in Loans)
+            {
+                Loan tempLoan = new Loan(l.id);
+                tempLoan.principal = l.principal;
+                tempLoan.interest = l.interest;
+                tempLoan.rate = l.rate;
+                payoffLoans.Add(tempLoan);
+            }
+
+            double paymentAmt = 0;
+            try
+            {
+                paymentAmt = Convert.ToDouble(PaymentAmt.Text);
+            } catch (FormatException ex)
+            {
+                ResultsText.Content = "Payoff Amount missing";
+                return;
+            }
+            bool paidOff = false;
+            int months = 0;
+            while(paidOff == false)
+            {
+                
+                PayMonth(paymentAmt, ref payoffLoans);
+                months++;
+                for(int i = 0; i < payoffLoans.Count; i++)
+                {
+                    Debug.WriteLine("Loan " + i + " principal: " + payoffLoans[i].principal);
+                    if(payoffLoans[i].principal == 0)
+                    {
+                        payoffLoans.RemoveAt(i);
+                    }
+                }
+                if(payoffLoans.Count == 0)
+                {
+                    paidOff = true;
+                    ResultsText.Content = "You will payoff your loans in " + months / 12 + " years and " + months % 12 + " months.";
+                }
+            }
+        }
+
+        private void PayMonth(double payment, ref List<Loan> currentLoans)
+        {
+            
+            for (int i = 0; i < currentLoans.Count; i++)
+            {
+                //TODO do math to make sure payment - interest is not negative.  if negative, only apply what can be applied.
+                if(payment > currentLoans[i].interest)
+                {
+                    payment -= currentLoans[i].interest;
+                    currentLoans[i].interest = 0;
+                } else
+                {
+                    //Apply whats left
+                    currentLoans[i].interest -= payment;
+                    payment = 0;
+
+                    break;
+                }
+                
+            }
+            //divide remaining payment evenly across principals
+            double splitPaymentEvenly = payment / currentLoans.Count;
+            for (int i = 0; i < currentLoans.Count; i++)
+            {
+                currentLoans[i].principal -= splitPaymentEvenly;
+                if(currentLoans[i].principal <= 0)
+                {
+                    currentLoans[i].principal = 0;
+                    continue; // don't calculate interest, loan is done.
+                }
+                currentLoans[i].interest = currentLoans[i].principal * (currentLoans[i].rate / 1200);
+            }
+        }
+
+        /*
+         * Resets the input fields to their default state.
+         */
+        private void ResetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Loans.Clear();
+            this.Loans.Add(new Loan(1));
+
+            PaymentAmt.Text = "";
+            ResultsText.Content = "";
+        }
+    }
+}
---
+++ LoanPaymentCalculator/ViewModels/Loan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanPaymentCalculator.ViewModels
+{
+    /// <summary>
+    ///  View Model for a Loan entry row
+    /// </summary>
+    public class Loan 
+    {
+        public double principal { get; set; }
+        public double interest { get; set; }
+        public double rate { get; set; }
+        public int id { get; set; }
+
+        public Loan(int id)
+        {
+            principal = 0;
+            interest = 0;
+            rate = 0.00;
+            this.id = id;
+        }
+
+        public override string ToString()
+        {
+            return "Principal: " + principal + " - Interest: " + interest + " - Rate: " + rate;
+        }
+    }
+}
